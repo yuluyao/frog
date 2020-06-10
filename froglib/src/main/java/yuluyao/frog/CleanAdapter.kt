be94cdaece5d384f0eac9a3cccd9ce61d6f432f8
yuluyao <wushenggoo@gmail.com>
2020-06-10 11:18:37 +0800

    kotlin DSL support

--- froglib/src/main/java/yuluyao/frog/CleanAdapter.kt
+++ froglib/src/main/java/yuluyao/frog/CleanAdapter.kt
@@ -7,10 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.extensions.LayoutContainer
 
-abstract class CleanAdapter<T>(private val layoutId: Int = 0) : RecyclerView.Adapter<CleanAdapter.Holder>() {
+open class CleanAdapter<T>(private val layoutId: Int = 0) : RecyclerView.Adapter<CleanAdapter.Holder>() {
+  //<editor-fold desc="DSL support">
+  private var callback: (CleanAdapter<T>.(Holder, Int) -> Unit)? = null
+  operator fun invoke(block: CleanAdapter<T>.(Holder, Int) -> Unit) {
+    callback = block
+  }
+  //</editor-fold>
+
   var data: ArrayList<T> = arrayListOf()
   override fun getItemCount(): Int = data.size
 
+  // support multiple item types
   private var types: SparseIntArray? = null
   fun initItemViewType(typeArray: IntArray, layoutArray: IntArray) {
     types = SparseIntArray()
@@ -20,12 +28,17 @@ abstract class CleanAdapter<T>(private val layoutId: Int = 0) : RecyclerView.Ada
   }
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-    val inflater = LayoutInflater.from(parent.context)
-    val layout = types?.get(viewType) ?: layoutId
-    val rootView = inflater.inflate(layout, parent, false)
+    val rootView = LayoutInflater.from(parent.context).inflate(types?.get(viewType) ?: layoutId, parent, false)
     return Holder(rootView)
   }
 
+
+  override fun onBindViewHolder(holder: Holder, position: Int) {
+    callback?.invoke(this, holder, position)
+  }
+
   class Holder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
+
+
 }
 
--- frogsample/src/main/java/com/yuluyao/frog/func/adapter/AdapterActivity.kt
+++ frogsample/src/main/java/com/yuluyao/frog/func/adapter/AdapterActivity.kt
@@ -2,7 +2,6 @@ package com.yuluyao.frog.func.adapter
 
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.view.ViewGroup
 import com.yuluyao.frog.R
 import com.yuluyao.frog.base.BaseActivity
 import com.yuluyao.frog.repo.Data
@@ -12,32 +11,40 @@ import kotlinx.android.synthetic.main.base_item_data.view.*
 import yuluyao.frog.CleanAdapter
 
 class AdapterActivity : BaseActivity() {
-  private val adapter = object : CleanAdapter<Data>(R.layout.base_item_data) {
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-      val item = data[position]
-      holder.itemView.icon.setImageResource(item.iconRes)
-      holder.itemView.title.text = item.title
-      holder.itemView.content.text = item.content
-    }
-
-    override fun getItemViewType(position: Int): Int {
-      return super.getItemViewType(position)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-      return super.onCreateViewHolder(parent, viewType)
-
-    }
-  }
+//  private val adapter = object : CleanAdapter<Data>(R.layout.base_item_data) {
+////    override fun onBindViewHolder(holder: Holder, position: Int) {
+////      val item = data[position]
+////      holder.itemView.icon.setImageResource(item.iconRes)
+////      holder.itemView.title.text = item.title
+////      holder.itemView.content.text = item.content
+////    }
+//
+//    override fun getItemViewType(position: Int): Int {
+//      return super.getItemViewType(position)
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+//      return super.onCreateViewHolder(parent, viewType)
+//
+//    }
+//  }
 
   override fun onGetLayoutId(): Int = R.layout.base_activity_data
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     title = "Adapter"
 
+    val adapter = CleanAdapter<Data>(R.layout.base_item_data)
+    adapter { holder, position ->
+      val item = data[position]
+      holder.itemView.icon.setImageResource(item.iconRes)
+      holder.itemView.title.text = item.title
+      holder.itemView.content.text = item.content
+    }
+
     recyclerView.layoutManager = LinearLayoutManager(this)
     recyclerView.adapter = adapter
-    Repo.refresh().subscribe {it->
+    Repo.refresh().subscribe { it ->
       adapter.data = it
       adapter.notifyDataSetChanged()
     }
